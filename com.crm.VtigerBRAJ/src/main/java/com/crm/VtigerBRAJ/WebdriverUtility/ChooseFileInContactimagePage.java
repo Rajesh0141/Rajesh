816bf180afc118364c8e564f87cfcbeda7bd1523
TestScriptGitBash <rajeshbhukya473@gmail.com>
2022-05-10 10:27:03 +0530

    RjaeshCommit

---
+++ com.crm.VtigerBRAJ/src/main/java/com/crm/VtigerBRAJ/WebdriverUtility/ChooseFileInContactimagePage.java
@@ -0,0 +1,24 @@
+package com.crm.VtigerBRAJ.WebdriverUtility;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.crm.VtigerBRAJ.genericUtility.WebDriverUtility;
+
+
+
+public class ChooseFileInContactimagePage {
+	
+	public ChooseFileInContactimagePage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}	
+	public void ChooseFileContactImage(WebDriver driver,String partialWindowTitle) {
+		WebDriverUtility wLib=new WebDriverUtility();
+		wLib.switchToWindow(driver, partialWindowTitle);
+		WebElement uploadFile= driver.findElement(By.name("imagename_hidden"));
+		uploadFile.sendKeys("C:\\Users\\Rajesh Bhukya\\OneDrive\\Desktop\\Camera");
+	}
+}
